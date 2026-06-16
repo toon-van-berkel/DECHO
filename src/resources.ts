b5ad929a35b0bc3f711923466306d50aa1ae9fea
Casper <caspervgameren@outlook.com>
2026-06-16 12:00:20 +0200

    I added a dynamic, fully functional ResourceLoader to load all future images and or audio files. The default export is the entire loader

--- src/resources.ts
+++ src/resources.ts
@@ -1,14 +1,52 @@
-import { ImageSource, Loader } from "excalibur";
+import * as ex from 'excalibur';
 
-// It is convenient to put your resources in one place
+// Define Resource folders
 export const Resources = {
-  Sword: new ImageSource("./images/sword.png") // Vite public/ directory serves the root images
-} as const; // the 'as const' is a neat typescript trick to get strong typing on your resources. 
-// So when you type Resources.Sword -> ImageSource
+  Backgrounds: {} as Record<string, ex.ImageSource>,
+  Characters: {} as Record<string, ex.ImageSource>,
+  UIElements: {} as Record<string, ex.ImageSource>,
+}
 
-// We build a loader and add all of our resources to the boot loader
-// You can build your own loader by extending DefaultLoader
-export const loader = new Loader();
-for (const res of Object.values(Resources)) {
-  loader.addResource(res);
+// Define paths to asset folders within the assets folder
+const backgroundAssets = import.meta.glob('./assets/backgrounds/background-*.png', { eager: true, import: 'default' });
+const characterAssets = import.meta.glob('./assets/backgrounds/characters-*.png', { eager: true, import: 'default' });
+const uiElemetAssets = import.meta.glob('./assets/backgrounds/ui-*.png', { eager: true, import: 'default' });
+
+// Set the connection between all assets and their Resource folders
+const assetConnections = [
+  { assets: backgroundAssets, folder: Resources.Backgrounds },
+  { assets: characterAssets, folder: Resources.Characters },
+  { assets: uiElemetAssets, folder: Resources.UIElements },
+]
+
+// Load the asset path dynamically using its path and folder
+const loadAssetPath = (assetPath: any, assetFolder: any) => {
+  // Loop through all the assetPaths
+  for (const path in assetPath) {
+    // Define the origional path of the asset
+    const origionalPath = assetPath[path] as string;
+
+    // Define the new clean name of the asset e.g. house || city
+    const cleanPath = path
+      /** 
+       * RegEx / ... / look for text
+      * ^ Absolute start of the text which e.g. b in background
+      * .* Select all the text untill -
+      */
+      .replace(/^.*-/, '')
+      /**
+       * \. Looks for a dot
+       * [^.]+$ Get all characters that follow untill the absolute end of the text
+       */
+      .replace(/\.[^]+$/, '');
+
+    // Create a new ImageScourse based on the Resource.assetFolder[name]
+    assetFolder[cleanPath] = new ex.ImageSource(origionalPath);
+  }
+}
+
+// Loop though all connected assets
+for (const connection of assetConnections) {
+  // Load the assets based on the connected items
+  loadAssetPath(connection.assets, connection.folder);
 }
---
+++ src/scenes/scene-start.ts
@@ -0,0 +1,12 @@
+import * as ex from "excalibur";
+
+export default class SceneStart extends ex.Scene {
+
+    constructor() {
+        super();
+    }
+    
+    onInitialize(engine: ex.Engine): void {
+        
+    }
+}
