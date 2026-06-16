f4cac0181d0a34a793295a61b7fb2cd85072b42f
Casper <caspervgameren@outlook.com>
2026-06-16 14:43:58 +0200

    UPDATED main for a cleaner and more managable structure REMOVED level form main

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
-import { Color, DisplayMode, Engine, FadeInOut } from "excalibur";
+import * as ex from 'excalibur';
 import { loader } from "./resources";
 import { StartScene } from './scenes';
-import { MyLevel } from "./level";
 
 // Goal is to keep main.ts small and just enough to configure the engine
 
-export class Game extends Engine {
+export class Game extends ex.Engine {
   constructor() {
     super({
       width: 800, // Logical width and height in game pixels
       height: 600,
-      displayMode: DisplayMode.FitScreenAndFill, // Display mode tells excalibur how to fill the window
+      displayMode: ex.DisplayMode.FitScreenAndFill, // Display mode tells excalibur how to fill the window
       pixelArt: true, // pixelArt will turn on the correct settings to render pixel art without jaggies or shimmering artifacts
       scenes: {
         sceneStart: StartScene,
@@ -26,21 +25,7 @@ export class Game extends Engine {
       this.goToScene('sceneStart');
     })
   }
-
-  // physics: {
-  //   solver: SolverStrategy.Realistic,
-  //   substep: 5 // Sub step the physics simulation for more robust simulations
-  // },
-  // fixedUpdateTimestep: 16 // Turn on fixed update timestep when consistent physic simulation is important
 };
 
-game.start('start', { // name of the start scene 'start'
-  loader, // Optional loader (but needed for loading images/sounds)
-  inTransition: new FadeInOut({ // Optional in transition
-    duration: 1000,
-    direction: 'in',
-    color: Color.ExcaliburBlue
-  })
-}).then(() => {
-  // Do something after the game starts
-});
+const game = new Game();
+game.start();
